ce4e7b2cac833e306624bb92116e0d18554f0828
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-08-27 16:32:43 -0600

    Move TinyMCE into component

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,12 +1,7 @@
-import { Editor } from '@tinymce/tinymce-react';
+import TinyMCE from './TinyMCE';
 
 function NewNote() {
-  return (
-    <Editor
-      apiKey="bvw3vvjdka7tpr9z6r2zs15ms3h6c4hthn9803f5k8ddqw2r"
-      plugins="wordcount"
-    />
-  );
+  return <TinyMCE />;
 }
 
 export default NewNote;
---
+++ src/components/TinyMCE.js
@@ -0,0 +1,12 @@
+import { Editor } from '@tinymce/tinymce-react';
+
+function TinyMCE() {
+  return (
+    <Editor
+      apiKey="bvw3vvjdka7tpr9z6r2zs15ms3h6c4hthn9803f5k8ddqw2r"
+      plugins="wordcount"
+    />
+  );
+}
+
+export default TinyMCE;
