d552328df1e32869b6d01e29ba0e25a9cc220c49
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-08-27 14:36:19 -0600

    Create ContentListings component

---
+++ src/components/ContentListings.js
@@ -0,0 +1,21 @@
+import './ContentListings.css';
+import ContentCard from './ContentCard';
+
+function ContentListings() {
+  return (
+    <div className="contentListings">
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+    </div>
+  );
+}
+
+export default ContentListings;
