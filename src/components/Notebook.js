bc68d8642f9375fb1623cc04e51e85a4f5ee3d53
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-08-27 14:35:45 -0600

    Load ContentListings component

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,11 +1,13 @@
 import './Notebook.css';
 import SideNav from './SideNav';
+import ContentListings from './ContentListings';
 
 function Notebook() {
   return (
     <div>
       <SideNav />
       <p>Welcome to the notebook</p>
+      <ContentListings />
     </div>
   );
 }
