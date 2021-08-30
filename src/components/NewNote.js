2e1426fac5ef98ceb1f11e81d8269e7eddc4a957
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-08-30 10:10:46 -0600

    Add TinyMCE

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,7 +1,20 @@
+import React, { useRef } from 'react';
 import TinyMCE from './TinyMCE';
 
 function NewNote() {
-  return <TinyMCE />;
+  const editorRef = useRef(null);
+  const getData = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
+  return (
+    <div>
+      <TinyMCE editorRef={editorRef} />
+      <button onClick={getData}>Save</button>
+    </div>
+  );
 }
 
 export default NewNote;
--- src/components/TinyMCE.js
+++ src/components/TinyMCE.js
@@ -1,8 +1,9 @@
 import { Editor } from '@tinymce/tinymce-react';
 
-function TinyMCE() {
+function TinyMCE(props) {
   return (
     <Editor
+      onInit={(env, editor) => (props.editorRef.current = editor)}
       apiKey="bvw3vvjdka7tpr9z6r2zs15ms3h6c4hthn9803f5k8ddqw2r"
       plugins="wordcount"
     />
