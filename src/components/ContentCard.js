a8cf92856e535ff526ccc6a148369cd19c3174d6
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-21 14:12:13 -0600

    Add selection dispatch to notes

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,13 +1,18 @@
 import './ContentCard.css';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { add } from '../features/counter/selectSlice';
 
 function ContentCard(props) {
+  const dispatch = useDispatch();
+
   const createMarkup = () => {
     return { __html: props.note.body };
   };
 
   return (
     <li className="contentCard">
+      <input type="checkbox" onClick={() => dispatch(add(props.note._id))} />
       <Link to={'/notebook/' + props.note._id}>
         <div>{props.note.title}</div>
       </Link>
