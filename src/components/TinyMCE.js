a1c5f8ce30bbb672a069f7cf4fb743e0c45983e4
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-21 04:33:43 -0600

    Change TinyMCE to accept existing note data

--- src/components/TinyMCE.js
+++ src/components/TinyMCE.js
@@ -6,6 +6,7 @@ function TinyMCE(props) {
       onInit={(env, editor) => (props.editorRef.current = editor)}
       apiKey="bvw3vvjdka7tpr9z6r2zs15ms3h6c4hthn9803f5k8ddqw2r"
       plugins="wordcount"
+      initialValue={props.value}
     />
   );
 }
