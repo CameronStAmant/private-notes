99e968a71ad17a7948b6910ed1028bfc4e95de63
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-28 14:12:20 -0600

    Update checkboxToggle to include removing from arr

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,7 +1,7 @@
 import './ContentCard.css';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { add } from '../features/counter/selectSlice';
+import { add, remove } from '../features/counter/selectSlice';
 
 function ContentCard(props) {
   const dispatch = useDispatch();
@@ -10,9 +10,17 @@ function ContentCard(props) {
     return { __html: props.note.body };
   };
 
+  const checkboxToggle = (e) => {
+    if (e.target.checked) {
+      dispatch(add(props.note._id));
+    } else {
+      dispatch(remove(props.note._id));
+    }
+  };
+
   return (
     <li className="contentCard">
-      <input type="checkbox" onClick={() => dispatch(add(props.note._id))} />
+      <input type="checkbox" onClick={checkboxToggle} />
       <Link to={'/notebook/' + props.note._id}>
         <div>{props.note.title}</div>
       </Link>
