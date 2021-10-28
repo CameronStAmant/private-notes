3ea5570247cb19fa1b7e54af4b1c114c98dd929b
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-28 14:01:15 -0600

    Remove TopNav content to separate component

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,45 +1,16 @@
 import './Notebook.css';
-import SideNav from './SideNav';
 import ContentListings from './ContentListings';
-import Button from './Button';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import baseUrl from '../const';
 import React, { useState } from 'react';
+import Navigation from './Navigation';
 
 function Notebook() {
-  const view = useSelector((state) => state.nav.value);
-  const selected = useSelector((state) => state.selected.value);
-  const [refresh, setRefresh] = useState(false);
-
-  const multiDelete = async () => {
-    const requestOptions = {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ids: selected,
-      }),
-    };
-    await fetch(baseUrl + '/notebook/delete-many-notes', requestOptions);
-    setRefresh(refresh ? false : true);
-  };
-
   return (
-    <div>
-      <div className="topNav">
-        <SideNav />
-      </div>
-      <div className={view ? 'mainContentNavOpened' : 'mainContentNavClosed'}>
+    <Navigation>
+      <div>
         <p>Welcome to the notebook</p>
-        <Link to="/notes/new">
-          <Button wording="+" />
-        </Link>
-        <button onClick={multiDelete}>Delete selected</button>
-        <ContentListings refresh={refresh} />
+        <ContentListings />
       </div>
-    </div>
+    </Navigation>
   );
 }
 
