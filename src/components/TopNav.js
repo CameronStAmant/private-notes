33f7c1701e7c13e39e3c77cde8cbf3262c95423c
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-28 13:58:19 -0600

    Add TopNav

---
+++ src/components/TopNav.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { clear } from '../features/counter/selectSlice';
+import baseUrl from '../const';
+
+import Button from './Button';
+
+const TopNav = () => {
+  const selected = useSelector((state) => state.selected.value);
+  const [refresh, setRefresh] = useState(false);
+  const dispatch = useDispatch();
+
+  const multiDelete = async () => {
+    const requestOptions = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ids: selected,
+      }),
+    };
+    await fetch(baseUrl + '/notebook/delete-many-notes', requestOptions);
+    setRefresh(refresh ? false : true);
+    dispatch(clear());
+  };
+
+  return (
+    <div>
+      <Link to="/notes/new">
+        <Button wording="+" />
+      </Link>
+      <button onClick={multiDelete}>Delete selected</button>
+    </div>
+  );
+};
+
+export default TopNav;
