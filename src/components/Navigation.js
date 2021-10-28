2ed73464a180ea72fa35a6293d79deaa699b604e
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-28 13:58:30 -0600

    Combine SideNav and TopNav

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+
+import TopNav from './TopNav';
+import SideNav from './SideNav';
+
+const Navigation = (props) => {
+  const view = useSelector((state) => state.nav.value);
+
+  return (
+    <div>
+      <SideNav />
+      <div className={view ? 'mainContentNavOpened' : 'mainContentNavClosed'}>
+        <TopNav />
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
