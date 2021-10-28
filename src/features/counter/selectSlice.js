806006e290806ef842b3ff541eeb26766ea59d71
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-10-28 13:57:40 -0600

    Add remove and clear selectSlices

--- src/features/counter/selectSlice.js
+++ src/features/counter/selectSlice.js
@@ -9,9 +9,15 @@ export const selectSlice = createSlice({
     add: (state, action) => {
       state.value.push(action.payload);
     },
+    remove: (state, action) => {
+      state.value = state.value.filter((item) => item !== action.payload);
+    },
+    clear: (state) => {
+      state.value = [];
+    },
   },
 });
 
-export const { add } = selectSlice.actions;
+export const { add, remove, clear } = selectSlice.actions;
 
 export default selectSlice.reducer;
