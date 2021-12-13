3f53d9e7c9b321304af0b8f202bcd1a99e7922fe
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-12-13 16:21:01 -0700

    Update routing to be in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import routes from './Routes';
 import { useSelector } from 'react-redux';
-import { useRoutes } from 'react-router-dom';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Notebook from './components/Notebook';
+import Note from './components/Note';
+import NewNote from './components/NewNote';
+import Folder from './components/Folder';
+
+import Login from './components/Login';
+
+import { useRoutes, Routes, Route } from 'react-router-dom';
 
 function App() {
   const { isLoggedIn } = useSelector((state) => state.authentication);
-  const routing = useRoutes(routes(isLoggedIn));
+  // const routing = useRoutes(isLoggedIn);
 
-  return <>{routing}</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="notebook" element={<Notebook />} />
+      <Route path="notebook/:id" element={<Note />} />
+      <Route path="notebook/:id/edit" element={<NewNote />} />
+      <Route path="notebook/folders/:id" element={<Folder />} />
+      <Route path="notes/new" element={<NewNote />} />
+    </Routes>
+  );
 }
 
 export default App;
